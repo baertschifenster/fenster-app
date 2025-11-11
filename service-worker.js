68aa8264e144c958d79a9c3817e49aee64682392
baertschifenster <stefan.baertschi@bf-ag.ch>
2025-11-11 21:57:54 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,36 @@
-const CACHE = "fenster-app-v36";
+const CACHE_VERSION = "v3.9.1";   // <<< wenn du etwas änderst: Zahl erhöhen
+const CACHE_NAME = `fenster-app-${CACHE_VERSION}`;
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((cache) =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./manifest.json",
-        "./icons/icon-192.png",
-        "./icons/icon-512.png"
-      ])
-    )
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((k) => k !== CACHE)
-          .map((k) => caches.delete(k))
-      )
-    )
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
   );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(r => r || fetch(event.request))
   );
 });
